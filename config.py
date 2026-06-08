80177c1c24650ca405176adce806856028269bb6
infinitefusion <charles.dubois@doculibre.com>
2026-06-08 13:32:56 -0400

    fixes typo 2

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import json
 import os
 from pathlib import Path
 
-FOLDER_NAME = "infinitefusion-common"
+FOLDER_NAME = "infinitefusion_common"
 def _config_path() -> Path:
     base = Path(os.environ.get("APPDATA", Path.home()))
     return base / FOLDER_NAME / "launcher_config.json"
